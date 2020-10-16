7c466828bf453d4b33d2c38abc62a6e06f7de527
Dmytro Arkhypov <dimonarh@gmail.com>
2020-10-16 18:39:17 +0300

    added styles for search form (google)

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-let dialog = document.querySelector('dialog');
-document.querySelector('#contacts-show').onclick = function() {
-    dialog.showModal();
-};
-document.querySelector('#contacts-close').onclick = function() {
-    dialog.close();
-};
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
+window.onload = function() {
+    //Modal Pop-Up
+    let dialog = document.querySelector('dialog');
+    document.querySelector('#contacts-show').onclick = function() {
+        dialog.showModal();
+    };
+    document.querySelector('#contacts-close').onclick = function() {
+        dialog.close();
+    };
 
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open')
-        menuOpen = false;
-    }
-});
+//Burger Button toggle
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+
+    menuBtn.addEventListener('click', () => {
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open')
+            menuOpen = false;
+        }
+    });
+
+//Text for GoogleSearch button
+    const searchButton = document.querySelector('.gsc-search-button-v2');
+    console.log(searchButton);
+    searchButton.innerText = "Искать";
+    console.log(searchButton);
+}
